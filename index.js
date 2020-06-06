d19ed1cb7f22265e447a7c371e5635a29039030d
Shirin Muataz <shirinmuataz@gmail.com>
2020-06-06 02:06:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
 // Add your code here
-let formData = {
-  dogName: "Byron",
-  dogBreed: "Poodle"
-};
- 
-let configObj = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body: JSON.stringify(formData)
-};
- 
-fetch("http://localhost:3000/dogs", configObj);
