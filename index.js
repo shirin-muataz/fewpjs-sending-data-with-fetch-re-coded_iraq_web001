51e54322823c6c132b0904b959099a1e0ef7baf5
Shirin Muataz <shirinmuataz@gmail.com>
2020-06-06 02:36:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData (userName, userEmail)
+{
+  let configObj = {
+    method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify({
+  name:userName,
+  email:userEmail
+  })
+};
+  return fetch('http://localhost:3000/users',configObj)
+  .then(resp=>{ return resp.json()})
+  .then (resp=> {
+    document.body.innerHTML=resp['id'];
+  })
+  .catch ( err=> { 
+    const p=document.createElement('p');
+    p.innerHTML=err.message
+    document.body.appendChild(p);
+    console.log(err.message);
+  })
+}
