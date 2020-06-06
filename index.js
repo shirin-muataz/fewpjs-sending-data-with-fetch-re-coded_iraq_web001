c8d0faf132185642f2797c24462a90b289e85d23
Shirin Muataz <shirinmuataz@gmail.com>
2020-06-06 02:11:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // Add your code here
 function submitData (userName, userEmail)
 {
-  
-  return (fetch('http://localhost:3000/users'),)
+  let configObj = {
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify({
+    name: userName,
+    emai:userEmail
+  })
+};
+  return (fetch('http://localhost:3000/users'),configObj)
 }
